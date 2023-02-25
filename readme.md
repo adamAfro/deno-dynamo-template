6f3fd03ee3ebf80f7fd2a17ce8d06d27f058be9d
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2023-02-25 22:49:46 +0100

    poprawki i czytelność

--- readme.md
+++ readme.md
@@ -6,6 +6,5 @@ Zamysł jest taki żeby bez zbędnych plików wyświetlać typescript w przeglą
 * projekt na razie nie robi nic
 * serwer wysyła zbundlowane (`esbuild`) skrypty we wnętrzu HTMLa z podstawową stroną
 * testy można prowadzić w konsoli dzięki `JSDOM`
-* assety może działają może nie, sprawdzi się jak się przydadzą
 
 Zamysł jest taki, żeby to wykorzystać do robienia gierki w `.ts`ie
--- serve.ts
+++ serve.ts
@@ -1,3 +1,99 @@
+// @example deno run --config ./ts.json -A serve.ts --dev
+// deno run --config ./ts.json -A serve.ts
+new class Server {
+
+    /** tells if server should rebundle app each clientpage-reload */
+    devmode: boolean = false
+
+    /** local ip address to connect from other devices */
+    ip: string | undefined
+
+    /** image string of avaible QR to scan */
+    qrcode: QRCode | undefined
+
+    /** bundled javascript code of the app */
+    private bundled: string | undefined
+
+    /** handles all connections */
+    async serve(port = 8000) {
+
+        this.devmode = Deno.args.some(arg => arg == "--dev")
+        this.bundled = await this.bundle()
+        this.ip = await getNetworkAddr()
+        this.qrcode = await qrcodify(this.ip + ":" + port)
+
+        for await (const conn of Deno.listen({ port }))
+            this.handle(conn)
+    }
+
+    /** bundles the app into javascript module code */
+    async bundle() {
+
+        const bundle = await esbuild.build({ 
+            write: false, bundle: true, 
+            entryPoints: ["./src/main.ts"],
+            format: 'esm', minify: true
+        })
+
+        return bundle.outputFiles[0].text
+    }
+
+    /** Response with the app */
+    async getApp() {
+
+        return new Response(html`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                <script type="module">${
+                    this.devmode ? await this.bundle() : this.bundled
+                }</script>
+            </head>
+            <body>
+                <img src="${this.qrcode}">
+            </body>
+            </html>`, { headers: { "content-type": contentType("html") } }
+        )
+    }
+
+    /** Gets a response with given assets in asset directory */
+    async getAsset(path: string, extension = <string> contentType(path.slice(path.lastIndexOf('.')))) {
+
+        return new Response(await Deno.readFile("./assets/" + path), { 
+            headers: <HeadersInit> { "content-type":  contentType(extension) }
+        })
+    }
+
+    async handle(conn: Deno.Conn) {
+
+        for await (const { respondWith, request } of Deno.serveHttp(conn)) try {
+
+            const path = new URL(request.url).pathname
+            console.info(path, request.method);
+
+            if (path == "/")
+                respondWith(this.getApp())
+            else
+            if (path.startsWith("/favicon.ico")) 
+                respondWith(this.getAsset("favicon.ico"))
+            else
+            if (path.startsWith("/assets/")) 
+                respondWith(this.getAsset(path.slice("/assets/".length)))
+            else
+                respondWith(new Response(null, { status: 404 }))
+
+        } catch (error: any) {
+
+            console.warn(error);
+            respondWith(new Response(null, { status: 400 }))
+        }
+    }
+
+} () .serve()
 //     _       ___     ___
 //    | |     |_ _|   | _ )    ___
 //    | |__    | |    | _ \   (_-<
@@ -6,60 +102,8 @@
 //  "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
 import { contentType } from "mediatypes"
 import * as esbuild from "esbuild"
-
-/** Bundling - adding -dev option will make app rebuild each refresh */
-let bundle: Function | string = async () => (await esbuild.build({ write: false,
-    bundle: true, entryPoints: ["./src/main.ts"], format: 'esm', minify: true
-})).outputFiles[0].text
-if (Deno.args[0] == "--dev")
-    bundle = await bundle()
+import { getNetworkAddr } from "iplocally"
+import { qrcode as qrcodify } from "qrcode"
 
 // HTML syntax highlighting tag
 const html = (strings: any, ...values: any) => String.raw({ raw: strings }, ...values)
-
-//                                      _          (")
-//     ___     ___      _ _   __ __    (_)    _ _   \|
-//    (_-<    / -_)    | '_|  \ V /    | |   | ' \
-//    /__/_   \___|   _|_|_   _\_/_   _|_|_  |_||_|   (g)
-//  _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|
-//  "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
-/** 
- * @example deno run --config ./ts.json --allow-read=./ --allow-net=0.0.0.0:8000 serve.ts # will need some additional access
- * @example deno run --config ./ts.json -A serve.ts # allow all wildcard
- * @example deno run --config ./ts.json -A serve.ts --dev
- */
-for await (const conn of Deno.listen({ port: 8000 })) for await (const { respondWith, request } of Deno.serveHttp(conn)) {
-
-    const path = new URL(request.url).pathname
-    if (path == "/") respondWith(new Response(
-
-        html`
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-                <script type="module">${typeof bundle == 'function' ? await bundle() : bundle}</script>
-            </head>
-            <body>
-
-            </body>
-            </html>`, {
-            headers: { "content-type": "text/html; charset=utf-8" },
-        }
-    ))
-
-    else if (path.startsWith("/assets/")) try {
-
-        const type = contentType(path.slice(path.lastIndexOf('.'))) || ""
-        respondWith(new Response(await Deno.readTextFile(path), {
-            headers: { "content-type": type }
-        }))
-
-    } catch (error: any) {
-
-        respondWith(new Response("", { status: 404 }))
-    }
-}
